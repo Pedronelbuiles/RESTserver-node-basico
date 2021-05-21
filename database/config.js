f46077b5791987d31e2b45aff90e24803e282ce9
pedro nel builes moreno <pedronel.builes@gmail.com>
2021-05-21 00:05:16 -0500

    Agregando funcion cerrar coneccion a bd

--- database/config.js
+++ database/config.js
@@ -18,7 +18,20 @@ const dbConnection = async () => {
     }
 }
 
+//Se debe cerrar la conección a la base de datos despues de obtener los datos y mostrarlos
+ //Esto con el fin de asegurarnos de que todas la conecciones que se habran a la base de datos se cierren de manera correcta
+const dbCloseConnection = async () => {
+    try {
+        await mongoose.connection.close()
+
+        console.log('Conección a la base de datos cerrada');
+    } catch (error) {
+        console.log(error)
+        throw new Error('Error al cerrar la base de datos'.red)
+    }
+}
 
 module.exports = {
-    dbConnection
+    dbConnection,
+    dbCloseConnection
 }
